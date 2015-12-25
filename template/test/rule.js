9b553120e1d089b04b7266cffe04aa39fa0d21e2
yumyfung <yumy.fung@gmail.com>
2015-12-25 09:39:05 +0800

    update

--- template/test/rule.js
+++ template/test/rule.js
@@ -6,8 +6,8 @@
 =============================================================*/
 
 module.exports.rule = {
-	name: '创建分屏滑动活动模板',
-	dir: ['E:/SvnProject/html/移动终端/活动类', 'E:/SvnProject/mediastyle/mobile/event'],
-	command: 'template_slide',
-	replaceLink: ['<link rel="stylesheet" href="index.css" />','<link rel="stylesheet" href="http://imgcache.gtimg.cn/mediastyle/mobile/event/']
+	name: '测试模板',
+	dir: ['E:/test/html', 'E:/test/style'],
+	command: 'template_test',
+	replaceLink: ['<link rel="stylesheet" href="index.css" />','<link rel="stylesheet" href="http://xxx.com/style/']
 }
